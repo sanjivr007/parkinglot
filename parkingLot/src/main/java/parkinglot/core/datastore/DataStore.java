d116bddef461235c134aad2d51c9906d4cce9505
Sanjiv ranjan <sanjivranjan@arvindinternet.com>
2017-12-09 18:08:20 +0530

    src code development

--- parkingLot/src/main/java/parkinglot/core/datastore/DataStore.java
+++ parkingLot/src/main/java/parkinglot/core/datastore/DataStore.java
@@ -1,21 +1,28 @@
 package main.java.parkinglot.core.datastore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import main.java.parkinglot.core.parkinglot.*;
 
 public class DataStore implements IDataStore {
+	private Map<String, ParkingLot> parkingLots = new HashMap<>();
+	  @Override
+	  public Optional<ParkingLot> getParkingLotByName(String name) {
+	    if (parkingLots.containsKey(name)) {
+	      return Optional.of(parkingLots.get(name));
+	    }
+	    return Optional.empty();
+	  }
 
-	@Override
-	public Optional<ParkingLot> getParkingLotByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	  @Override
+	  public void addParkingLot(ParkingLot parkingLot) {
+	    parkingLots.put(parkingLot.getName(), parkingLot);
+	  }
 
-	@Override
-	public void addParkingLot(ParkingLot parkingLot) {
-		// TODO Auto-generated method stub
-		
-	}
+	  public Map<String, ParkingLot> getParkingLots() {
+	    return parkingLots;
+	  }
 
 }
